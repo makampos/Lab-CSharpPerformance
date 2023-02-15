0150e29ee5176426cd4635f28bcd0cda9ac30942
Matheus <matheus.campos@edu.unifil.br>
2023-02-15 21:21:31 +0100

    string builder benchmark

---
+++ src/CsharpPerformance/CSharpPerformance.Tests/StringTests.cs
@@ -0,0 +1,72 @@
+namespace CSharpPerformance.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void BuildStringBadly()
+    {
+        var s = new StringWorker().BuildStringBadly("test");
+        Assert.Equal("test test test test test test test test test test",s);
+    }
+    
+    [Fact]
+    public void BuildStringBetter()
+    {
+        var s = new StringWorker().BuildStringBetter("test");
+        Assert.Equal("test test test test test test test test test test",s);
+    }
+    
+    [Fact]
+    public void NainveSplitName()
+    {
+        var name = "Behrens, Chris";
+        var worker = new StringWorker();
+        
+        var lastName = worker.NaiveSplitName(name).LastName;
+        var firstName = worker.NaiveSplitName(name).FirstName;
+        
+        Assert.Equal("Behrens",lastName);
+        Assert.Equal("Chris",firstName);
+    }
+    
+    [Fact]
+    public void SplitSplitName()
+    {
+        var name = "Behrens, Chris";
+        var worker = new StringWorker();
+        
+        var lastName = worker.SplitSplitName(name).LastName;
+        var firstName = worker.SplitSplitName(name).FirstName;
+        
+        Assert.Equal("Behrens",lastName);
+        Assert.Equal("Chris",firstName);
+    }
+    
+    [Fact]
+    public void FunctionalAreEquivalent()
+    {
+        var name = "Behrens, Chris";
+        var worker = new StringWorker();
+        
+        var lastName = worker.SplitSplitName(name).LastName;
+        var firstName = worker.SplitSplitName(name).FirstName;
+        
+        Assert.Equal("Behrens",lastName);
+        Assert.Equal("Chris",firstName);
+    }
+    
+    [Fact]
+    public void SpanSplitName()
+    {
+        var name = "Behrens, Chris";
+        var worker = new StringWorker();
+        
+        var lastName = worker.SpanSplitName(name).LastName;
+        var firstName = worker.SpanSplitName(name).FirstName;
+        
+        Assert.Equal("Behrens",lastName);
+        Assert.Equal("Chris",firstName);
+    }
+    
+    
+}
--- src/CsharpPerformance/CSharpPerformance/StringWorker.cs
+++ src/CsharpPerformance/CSharpPerformance/StringWorker.cs
@@ -6,7 +6,7 @@ public class StringWorker
 {
     public string BuildStringBadly(string value)
     {
-        for (var i = 0; i < 50; i++)
+        for (var i = 0; i < 9; i++)
         {
             value += " " + "test";
         }
@@ -17,12 +17,37 @@ public class StringWorker
     public string BuildStringBetter(string value)
     {
         var sb = new StringBuilder();
-        for (var i = 0; i < 50; i++)
+        for (var i = 0; i < 9; i++)
         {
             sb.Append(" ");
             sb.Append("test");
         }
 
         return sb.ToString();
+    }
+
+    public (string LastName, string FirstName) NaiveSplitName(string name)
+    {
+        var commaIndex = name.IndexOf(',');
+        var lastName = name.Substring(0, commaIndex);
+        var firstName = name.Substring(commaIndex + 2).Trim();
+
+        return (lastName, firstName);
+    }
+
+    public (string LastName, string FirstName) SplitSplitName(string name)
+    {
+        var nameArray = name.Split(',');
+        return (nameArray[0].Trim(), nameArray[1].Trim());
+    }
+    
+    public (string LastName, string FirstName) SpanSplitName(string name)
+    {
+        ReadOnlySpan<char> nameSpan = name.AsSpan();
+        
+        var lastName = nameSpan.Slice(0, nameSpan.IndexOf(',')).ToString();
+        var firstName = nameSpan.Slice(nameSpan.IndexOf(',') + 2).ToString().Trim();
+
+        return (lastName, firstName);
     }
 }
